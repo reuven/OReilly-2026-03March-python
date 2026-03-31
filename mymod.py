03be8fcacc046c69053161df2141fb3d1d7bfe6f
Reuven M. Lerner <reuven@lerner.co.il>
2026-03-31 20:42:07 +0300

    gitautosync update 54

--- mymod.py
+++ mymod.py
@@ -1,4 +1,4 @@
-if __name__ == '__main__':
+if __name__ == '__main__':   # if this is the first program run...
     print(f'Hello from {__name__}!')
 
 x = 100
@@ -8,5 +8,5 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello {name} from mymod!'
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # if this is the first program run...
     print(f'Goodbye from {__name__}!')
