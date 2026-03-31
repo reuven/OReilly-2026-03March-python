ee6ab970e599280cbd0dbdced907f36e9bb4e056
Reuven M. Lerner <reuven@lerner.co.il>
2026-03-31 20:53:35 +0300

    gitautosync update 60

---
+++ menu.py
@@ -0,0 +1,8 @@
+def menu(options):
+    while True:
+        user_choice = input(f'Choose from {options}: ').strip()
+    
+        if user_choice in options:   # did they choose something valid?
+            return user_choice
+
+        print(f'{user_choice} is invalid; try again!')
